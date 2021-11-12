c050aeb72bb5e72f6eede36f936b16b438efa5c9
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-12 03:22:51 -0500

    importing files from pages folder and useState from REACT

---
+++ src/components/portfolioContainer.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
