10b1a0e358307c4ff158f4ebfc04aad0c68763eb
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-12 03:43:12 -0500

    creating function that handles conditional rendering

--- src/components/portfolioContainer.js
+++ src/components/portfolioContainer.js
@@ -2,5 +2,19 @@ import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Home from './pages/Home';
 import About from './pages/About';
-import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+
+export default PortfolioContainer = () => {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if(currentPage === "Home") {
+        return <Home/>
+    }else if(currentPage === "About") {
+        return <About/>
+    }else {
+        return <Contact/>
+    }
+  }
+}
