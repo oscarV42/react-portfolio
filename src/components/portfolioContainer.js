745098f83f505c345000ce803ce7af787631efd1
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-12 03:44:46 -0500

    creating method that checks value of currentPage, the corresponding component will be returned to render

--- src/components/portfolioContainer.js
+++ src/components/portfolioContainer.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
+import NavBar from './NavBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
