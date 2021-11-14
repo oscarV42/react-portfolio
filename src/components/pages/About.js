2a4b7aa5641a1c334f98a68e344840efc4914a5e
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-13 23:47:06 -0500

    coding JSX html content to render a page about myself

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+    return (
+      <section className="about-me container-fluid" id="about-me">
+        <div className="container">
+          <h2>About Me</h2>
+
+            <p>
+            I'm a hispanic web developer currently enrolled in Georgia Tech coding
+            Bootcamp. Technologies I use for web development are HTML, CSS,
+            JavaScript/JQuery, Express.js, Node.js, and MYSQL. I have some
+            experience in Visual Basics and C/C++ from attending Edmonds Community
+            College. Web development is an art and I love to see all the progress
+            from creating and collaborating on projects. I don't have any
+            professional work experience in this field but I'm open to new
+            opportunities.
+            </p>
+        </div>
+      </section>
+    )
+}
