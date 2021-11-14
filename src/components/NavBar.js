26d879b600a55595f185b45695d6930261804caa
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-13 23:43:28 -0500

    editing navbar to be responsive using bootstrap classes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,36 +2,46 @@ import React from "react";
 
 const NavBar = ({ currentPage, handlePageChange }) => {
   return(
-    <ul className = "nav nav-bar">
-      <li className = "nav-item">
-        <a href = "#home" 
-        onClick={()=> handlePageChange('Home')}
-        className ={currentPage === "Home" ? 'nav-link active' : "nav-link"}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand mr-auto" href="#home">🕶️</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className = "navbar-nav ml-auto float-end mb-2 mb-lg-0">
+          <li className = "nav-item">
+            <a href = "#home" 
+            onClick={()=> handlePageChange('Home')}
+            className ={currentPage === "Home" ? 'nav-link active' : "nav-link"}
+            >
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#about"
+              onClick={() => handlePageChange('About')}
+              // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   )
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Home() {
   return (
     <div className= "container">
-      <h1>Hello 👋</h1>      
+      <h1>Welcome 👋</h1> 
+      <p>My name is Oscar Vizcaino and I'm from Peachtree City, Georgia. Feel free to navigate around and look at my qualifications and work.</p>     
     </div>
   )
 }
