9dbdc2db4ef52fee411e52886aa927a5a803c8e3
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-15 16:12:14 -0500

    merge conflict

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ const NavBar = ({ currentPage, handlePageChange }) => {
   <header id="header" className="fixed-top">
     <div className="container d-flex align-items-center justify-content-between">
       <h1 className="logo"><a href="#home" onClick={()=> handlePageChange('Home')}>🕶️</a></h1>
-<<<<<<< HEAD
       <nav className="navbar navbar-expand-lg" id= "navbar">
         <div className="container-fluid">
           <button className="navbar-toggler navbar-dark bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -45,40 +44,6 @@ const NavBar = ({ currentPage, handlePageChange }) => {
           </div>
         </div>
             {/* <i className="bi bi-list mobile-nav-toggle"></i> */}
-=======
-      <nav className="navbar" id= "navbar">
-            <ul>
-              <li className = "nav-item">
-                <a href = "#home" 
-                onClick={()=> handlePageChange('Home')}
-                className ={currentPage === "Home" ? 'nav-link active' : "nav-link"}
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="#about"
-                  onClick={() => handlePageChange('About')}
-                  // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                  className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                >
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="#work"
-                  onClick={() => handlePageChange('Work')}
-                  // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                  className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
-                >
-                  My Work
-                </a>
-              </li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
->>>>>>> parent of 072e06b (Updates)
       </nav>
     </div>
   </header>
