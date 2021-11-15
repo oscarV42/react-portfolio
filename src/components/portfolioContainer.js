7708e337e9e4f541c14a4abb5fb72660e8de8dba
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-15 16:11:04 -0500

    merge conflict

--- src/components/portfolioContainer.js
+++ src/components/portfolioContainer.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import NavBar from './NavBar';
-<<<<<<< HEAD
 import Footer from './Footer'
-=======
->>>>>>> parent of 072e06b (Updates)
 import Home from './pages/Home';
 import About from './pages/About';
 import Work from './pages/Work';
@@ -14,7 +11,6 @@ export default function PortfolioContainer() {
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if(currentPage === "Home") {
-<<<<<<< HEAD
         return (
         <main id="main">
           <Home/>
@@ -32,13 +28,6 @@ export default function PortfolioContainer() {
           <Work/>
         </main>
         )
-=======
-        return <Home/>
-    }else if(currentPage === "About") {
-        return <About/>
-    }else {
-        return <Work/>
->>>>>>> parent of 072e06b (Updates)
     }
   }
 
@@ -50,10 +39,7 @@ export default function PortfolioContainer() {
       <NavBar currentPage = { currentPage } handlePageChange = { handlePageChange }/>
       {/* Calling the renderPage method which will return a component */}
       {renderPage()}
-<<<<<<< HEAD
       <Footer />
-=======
->>>>>>> parent of 072e06b (Updates)
     </div>
   );
 }
