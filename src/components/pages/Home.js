58e6e56d884faa5f7ba1342b320e243eb9e78117
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-15 14:30:13 -0500

    implementing Typed.js js animation to home screen

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import Typed from 'typed.js';
 
 export default function Home() {
+
+  // Create Ref element.
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Designer", "Developer"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 150,
+      backSpeed: 150,
+      backDelay: 100,
+      loop: true,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <div
     id="hero"
@@ -15,8 +36,7 @@ export default function Home() {
           <h1 className="hero-title mb-4">I am Oscar Vizcaino</h1>
           <p className="hero-subtitle">
             <span
-              className="typed"
-              data-typed-items="Designer, Developer"
+              ref={el}
             ></span>
           </p>
         </div>
